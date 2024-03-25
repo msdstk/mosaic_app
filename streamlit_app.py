a0623dfd377b4d50c20eeb0b5e8844896c34318e
msdstk <155515428+msdstk@users.noreply.github.com>
2024-03-25 22:42:48 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
-import cv2
 import streamlit as st
+from streamlit_webrtc import webrtc_streamer
 import numpy as np
+import cv2
 
 def apply_mosaic(image, faces):
     for (x, y, w, h) in faces:
@@ -13,24 +14,19 @@ def apply_mosaic(image, faces):
 def main():
     st.title("Real-time Face Mosaic App📸")
 
-    # カメラの起動
-    cap = cv2.VideoCapture(0)
-
-    # 顔検出用の分類器を読み込み
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
-    while True:
-        # カメラからフレームを取得
-        ret, frame = cap.read()
-
+    def transform(frame):
+        # 顔検出用の分類器を読み込み
+        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+        
         # 顔を検出
         faces = face_cascade.detectMultiScale(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), scaleFactor=1.3, minNeighbors=5)
 
         # 検出された顔にモザイクをかける
         frame_with_mosaic = apply_mosaic(frame.copy(), faces)
 
-        # Streamlitウィジェットにフレームを表示
-        st.image(frame_with_mosaic, caption="Mosaic Applied Image", use_column_width=True)
+        return frame_with_mosaic
+
+    webrtc_streamer(key="example", video_transformer_factory=transform)
 
 if __name__ == "__main__":
     main()
